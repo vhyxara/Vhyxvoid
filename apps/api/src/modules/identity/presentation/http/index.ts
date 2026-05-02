0c78a6136b05723f94e66a15c09e0f7329468606
tanveeeer-in <shaikhtanveer5205@gmail.com>
2026-05-02 15:42:58 +0530

    tunnel main register

--- apps/api/src/modules/identity/presentation/http/index.ts
+++ apps/api/src/modules/identity/presentation/http/index.ts
@@ -7,6 +7,7 @@ import { adminRoutes } from "@/modules/identity/presentation/http/admin/admin.ro
 import { billingRoutes } from "@/modules/billing/presentation/http/billing.routes";
 import { stripeWebhookRoutes } from "@/modules/billing/presentation/http/webhook.routes";
 import { notificationRoutes } from "@/modules/notification/presentation/http/notification.routes";
+import { tunnelRoutes } from "./user/tunnel.routes";
 
 const registerRoutes = async (server: FastifyInstance) => {
   await server.register(identityRoutes, { prefix: "/api/v1/auth" });
@@ -15,7 +16,7 @@ const registerRoutes = async (server: FastifyInstance) => {
 
   await server.register(adminRoutes, { prefix: "/api/v1/admin/identity" });
 
-  // await server.register(tunnelRoutes, { prefix: "/api/v1/tunnel" });
+  await server.register(tunnelRoutes, { prefix: "/api/v1/tunnel" });
 
   await server.register(billingRoutes, { prefix: "/api/v1/billing" });
 
